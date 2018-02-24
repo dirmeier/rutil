022cc18a21f910d7b567f8abce2cfde907aa60ab
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-02-25 00:42:51 +0100

    Fixes

--- R/themes.R
+++ R/themes.R
@@ -28,45 +28,51 @@ slides_theme <- function()
     ggplot2::theme_minimal() +
       ggplot2::theme(
         text = ggplot2::element_text(size=16, family="Arial Narrow"),
-        axis.title.x = ggplot2::element_text(size=30),
-        axis.title.y = ggplot2::element_text(size=30),
-        axis.text.x = ggplot2::element_text(size=25),
-        axis.text.y = ggplot2::element_text(size=25),
-        plot.title = ggplot2::element_text(size=30),
-        legend.text = ggplot2::element_text(size=25),
-        legend.title = ggplot2::element_text(size=30),
-        strip.text=element_text(size=25)
-        panel.grid.minor = element_blank())
+        axis.title.x     = ggplot2::element_text(size=30),
+        axis.title.y     = ggplot2::element_text(size=30),
+        axis.text.x      = ggplot2::element_text(size=25),
+        axis.text.y      = ggplot2::element_text(size=25),
+        plot.title       = ggplot2::element_text(size=30),
+        legend.text      = ggplot2::element_text(size=25),
+        legend.title     = ggplot2::element_text(size=30),
+        strip.text       = ggplot2::element_text(size=25),
+        panel.grid.minor = ggplot2::element_blank())
 }
+
 
 #' @title Color palette
 #'
 #' @description Collection of discrete color palettes
 #' @export
-#' @import methods
+#' @param set  the color set to use
+#' @param alpha  alpha
 manual_discrete_colors <- function(set=c("a"), alpha=1)
 {
-    set <- methods::match.arg(set)
+    set <- match.arg(set)
     switch(set,
       "a"=c("#111111", "#65ADC2", "#233B43", "#E84646", "#C29365", "#362C21", "#316675", "#168E7F", "#109B37")
     )
 }
 
-#' @title Fill scale
-#'
-#' @description Several fill scalings.
-#' @export
-#' @import ggsci
-scale_fill_continuous <- function(set="blue")
-{
-    ggsci::scale_fill_material(set)
-}
 
 #' @title Fill scale
 #'
 #' @description Several fill scalings.
 #' @export
 #' @import ggsci
+#' @param set  the color set to use
+scale_fill_continuous <- function(set="blue")
+{
+    ggsci::scale_fill_material(set)
+}
+
+
+#' @title Color scale
+#'
+#' @description Several color scalings.
+#' @export
+#' @import ggsci
+#' @param set  the color set to use
 scale_color_continuous <- function(set="blue")
 {
     ggsci::scale_color_material(set)
