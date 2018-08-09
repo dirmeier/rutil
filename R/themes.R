1e9903af75c7550ce00777875e0409310dbf3211
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-08-09 18:25:54 +0200

    Adds jana theme

--- R/themes.R
+++ R/themes.R
@@ -40,6 +40,55 @@ slides_theme <- function()
 }
 
 
+#' @title Theme for paper plots
+#'
+#' @description This theme changes font sizes for plots uses in papers
+#' @export
+#' @import ggplot2
+paper_theme <- function()
+{
+    ggplot2::theme_minimal() +
+        ggplot2::theme(
+            text = ggplot2::element_text(size=16, family="Arial Narrow"),
+            axis.title.x     = ggplot2::element_text(size=10),
+            axis.title.y     = ggplot2::element_text(size=30),
+            axis.text.x      = ggplot2::element_text(size=25),
+            axis.text.y      = ggplot2::element_text(size=25),
+            plot.title       = ggplot2::element_text(size=30),
+            legend.text      = ggplot2::element_text(size=25),
+            legend.title     = ggplot2::element_text(size=30),
+            strip.text       = ggplot2::element_text(size=25),
+            panel.grid.minor = ggplot2::element_blank())
+}
+
+#' @title Theme adopted from Jana
+#'
+#' @description This theme changes font sizes for plots used in papers
+#' @export
+#' @import ggplot2
+jana_theme <- function(title.hjust = 0, legend_pos="bottom")
+{
+    ggplot2::theme(
+        axis.text    = ggplot2::element_text(size = 8),
+        axis.title.x = ggplot2::element_text(size = 8, face = "bold",
+                                             hjust = 1),
+        axis.title.y = ggplot2::element_text(size = 8, face = "bold"),
+        plot.title   = ggplot2::element_text(size = 8, face = "bold",
+                                             hjust = title.hjust),
+        plot.margin  = ggplot2::rep(grid::unit(1, "cm"), 4),
+        strip.text.x = ggplot2::element_text(size = 8),
+        strip.text.y = ggplot2::element_text(size = 8),
+        axis.line    = ggplot2::element_blank(),
+        legend.position = legend_pos,
+        legend.text  = ggplot2::element_text(size = 8),
+        legend.title = ggplot2::element_text(size = 8)) +
+    cowplot::background_grid(
+            major = "y", minor = "y",
+            colour.major = "grey80", colour.minor = "grey90",
+            size.major = 0.2, size.minor = 0.2)
+}
+
+
 #' @title Color palette
 #'
 #' @description Collection of discrete color palettes
@@ -50,7 +99,8 @@ manual_discrete_colors <- function(set=c("a"), alpha=1)
 {
     set <- match.arg(set)
     switch(set,
-      "a"=c("#111111", "#65ADC2", "#233B43", "#E84646", "#C29365", "#362C21", "#316675", "#168E7F", "#109B37")
+      "a"=c("#111111", "#65ADC2", "#233B43", "#E84646", "#C29365",
+            "#362C21", "#316675", "#168E7F", "#109B37")
     )
 }
 
